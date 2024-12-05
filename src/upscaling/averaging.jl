1c7a9f0582f0960fa11212718ddc0551b3c9a034
DRollin <91886709+DRollin@users.noreply.github.com>
2024-12-05 12:35:09 +0100

    fix average_quantities

--- src/upscaling/averaging.jl
+++ src/upscaling/averaging.jl
@@ -1,13 +1,13 @@
-function average_bulk_quantities(aₛₒₗ::Vector, setup::PhaseSetup)
+function average_quantities(aₛₒₗ::Vector, setup::PhaseSetup)
     (; dh, cells, cv, material) = setup
     (; E, αᶜʰ, k, cʳᵉᶠ, M, μʳᵉᶠ) = material
 
     uₛₒₗ = evaluate_at_grid_nodes(dh, aₛₒₗ, :u)
     μₛₒₗ = evaluate_at_grid_nodes(dh, aₛₒₗ, :μ)
-    cₛₒₗ = evaluate_at_grid_nodes(dh, aₛₒₗ, :μ)
+    cₛₒₗ = evaluate_at_grid_nodes(dh, aₛₒₗ, :c)
 
     j̄  = zero(Tensor{1,dim})
-    c̄ = 0.0
+    c̄  = 0.0
     c̄₂ = zero(Tensor{1,dim})
     σ̄  = zero(SymmetricTensor{2,2})
 
@@ -18,18 +18,19 @@ function average_bulk_quantities(aₛₒₗ::Vector, setup::PhaseSetup)
         coords = getcoordinates(cc)
         for qp in 1:getnquadpoints(cv.μ)
             dΩ = getdetJdV(cv.μ, qp)
-            μ = function_value(cv.μ, qp, μₛₒₗ[celldofs(cc)])
-            u = function_value(cv.u, qp, uₛₒₗ[celldofs(cc)])
-            c = function_value(cv.u, qp, uₛₒₗ[celldofs(cc)])
-            ϵ  = function_symmetric_gradient(cv.u, qp, uₛₒₗ[celldofs(cc)])
-            ∇μ = function_gradient(cv.μ, qp, μₛₒₗ[celldofs(cc)])
-            #x_μ  = spatial_coordinate(cv.μ, qp, coords)
-            c̄  += (cʳᵉᶠ+(μ-μʳᵉᶠ)/k) * dΩ
-            #c̄₂ += (cʳᵉᶠ+(μ-μʳᵉᶠ)/k)*x_μ * dΩ
-            j̄  -= M⋅∇μ * dΩ
+            uₛₒₗᵉ = @view uₛₒₗ[celldofs(cc)]
+            μᵉ, uᵉ, cᵉ = uₛₒₗᵉ[dofrange(dh, :μ)], uₛₒₗᵉ[dofrange(dh, :u)], uₛₒₗᵉ[dofrange(dh, :c)]
+            μ = function_value(cv.μ, qp, μᵉ)
+            u = function_value(cv.u, qp, uᵉ)
+            c = function_value(cv.c, qp, cᵉ)
+            ϵ  = function_symmetric_gradient(cv.u, qp, uᵉ)
+            ∇μ = function_gradient(cv.μ, qp, μᵉ)
+            x  = spatial_coordinate(cv.μ, qp, coords)
+            c̄  += (cʳᵉᶠ+(μ-μʳᵉᶠ)/k)   * dΩ
+            c̄₂ += (cʳᵉᶠ+(μ-μʳᵉᶠ)/k)*x * dΩ
+            j̄  -= M ⋅ ∇μ * dΩ
             σ̄  += E ⊡ (ϵ - αᶜʰ(c̄ - cʳᵉᶠ)) * dΩ
-            
         end
     end
-    return σ̄ , j̄
+    return σ̄, c̄, c̄₂, j̄
 end

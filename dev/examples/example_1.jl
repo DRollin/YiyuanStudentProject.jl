1d82684d8b545c3c316a288367d876e6eded2100
Documenter.jl <documenter@juliadocs.github.io>
2025-02-20 08:07:44 +0000

    build based on faaabde

--- dev/examples/example_1.jl
+++ dev/examples/example_1.jl
@@ -1,9 +1,6 @@
 using YiyuanStudentProject
 
-function example(Δt, t_total)
-    #Preparation
-
-    #sub scale
+function sub_scale_pre()
     dim = 3
     P = Material{dim}(; G=4.5e5, K=8.0e5, η=0.5, cʳᵉᶠ=0.0, μʳᵉᶠ=0.0, θʳᵉᶠ=1.0, cᵐ=1.0, α=0.2)
     M = Material{dim}(; G=0.8e5, K=1.7e5, η=1.0, cʳᵉᶠ=0.0, μʳᵉᶠ=0.0, θʳᵉᶠ=1.0, cᵐ=1.0, α=0.0)
@@ -11,23 +8,33 @@ function example(Δt, t_total)
     ϕ = 0.3
     meshsize = 0.1
     grid = generate_rve_grid(; ϕ=ϕ, d=d, meshsize=meshsize, dx=(1.0, 1.0, 1.0))
+    return grid, P, M
+end
 
-    #macro scale
+function macro_scale_pre()
     element_number = (1, 1, 1)
     x̄_l = Vec{3, Float64}((-50, -50, -50))
     x̄_r = Vec{3, Float64}((50, 50, 50))
     grid_macro = generate_grid(Tetrahedron, element_number , x̄_l, x̄_r)
+    return grid_macro
+end
 
-    #solve
-    rve = RVE(grid, P, M)
+function solve(grid_rve, P, M, grid_macro, Δt, t_total)
+    rve = RVE(grid_rve, P, M)
     setup_rve = prepare_setup(rve)
     assemble_K_M_f!(setup_rve)
     bc = MacroBCParams(Vec{3}((0.01, 0.0, 0.0)), Vec{3}((0.0, 0.0, 0.0)), 1.0, 0.0, "left", "right")
     res, res_rve, setup = solve_macro_problem(grid_macro, setup_rve, rve, bc, Δt=Δt, t_total=t_total)
-
-    #file, fig, anim = animate_combined_result(res, res_rve, setup, file_name="Myresult.mp4", scale=1000.0)
 end
 
-example(1e-6, 1e-5)
+function example()
+    grid_rve, P, M = sub_scale_pre()
+    grid_macro = macro_scale_pre()
+    Δt = 1e-6
+    t_total = 1e-5
+    solve(grid_rve, P, M, grid_macro, Δt, t_total)
+end
+
+example()
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
--- dev/literate-examples/example_1.jl
+++ dev/literate-examples/example_1.jl
@@ -5,11 +5,11 @@
 # as well as required parameters for a transient chemo-mechanical problem
 #
 #
-# In this example, we will solve the transient linear chemo-mechanical problem in FE² framework that is discussed the documentation in a domain with particles ($\Omega^\text{P}$)
-# embedded in matrix ($\Omega^\text{M}$). Parameters for solving the problem is defined for both sub scale(RVE) and macro scale. A macro scale boundary condition is as well selected
-# for constrain the object.
+# In this example, we will solve the transient linear chemo-mechanical problem in a FE² framework. The mathmatical model can be found in the documentation. A domain with particles ($\Omega^\text{P}$)
+# embedded in matrix ($\Omega^\text{M}$) is prescribed. Parameters for solving the problem are defined for both sub scale (RVE) and macro scale. A macro scale boundary condition is as well selected
+# to constrain the object.
 #
-# A main function will defined which sets up everything and solve the problem with a given time step size and total computational time.
+# A main function will be defined which sets up everything and solves the problem with a given time step size and total simulation time.
 #
 #md # The full program, without comments, can be found in the next [section](@ref example_1-plain-program).
 #
@@ -17,29 +17,12 @@ using YiyuanStudentProject
 #
 # ## Preparation
 # ### Sub Scale (RVE)
-# Setting up the dimension, material parameters. The material parameters are characterized using simple values for now
-# with cataloged with `P` for particles and `M` for matrix. 
+# Setting up the dimension, material parameters. The material parameters are cataloged with `P` for particles and `M` for matrix and 
+# are characterized using simple values for now.
+# 
 #
-# Generate a grid with spherical inclusions for a 3D Representative Volume Element (RVE) in desired meshsize and a grid size `dx = (1.0, 1.0, 1.0)`
-#
-# ### Macro Scale
-# Setting up the grid parameters and generate a macro scale grid. 
-#
-#
-# ## Solve
-# Construct an object ``RVESetup`` named `setup_rve` with relevent infomation for solving the RVE problem.
-#
-# Due to the linearity of the problem, gloable stiffness matrix ``K`` and gloable mass matrix ``M`` as well as gloable right hand side vector ``f`` remain the same
-# through out the time stepping. Perform the assembly for constructing the system matrices K, M, and right hand side vector f.
-#
-# Create an appropriate boundary condition using object ``MacroBCParams``
-#
-# Solve the time dependent problem macro scale problem. A corresponding RVE would be solved at each quadrature point in macro scale problem. 
-# a visualization of results from both fine scale and macro scale problem can use ``animate_combined_result``
-function example(Δt, t_total)
-    #Preparation
-
-    #sub scale
+# Generate a grid with spherical inclusions for a 3D Representative Volume Element (RVE) in desired meshsize and grid size `dx = (1.0, 1.0, 1.0)`
+function sub_scale_pre()
     dim = 3
     P = Material{dim}(; G=4.5e5, K=8.0e5, η=0.5, cʳᵉᶠ=0.0, μʳᵉᶠ=0.0, θʳᵉᶠ=1.0, cᵐ=1.0, α=0.2)
     M = Material{dim}(; G=0.8e5, K=1.7e5, η=1.0, cʳᵉᶠ=0.0, μʳᵉᶠ=0.0, θʳᵉᶠ=1.0, cᵐ=1.0, α=0.0)
@@ -47,27 +30,46 @@ function example(Δt, t_total)
     ϕ = 0.3
     meshsize = 0.1
     grid = generate_rve_grid(; ϕ=ϕ, d=d, meshsize=meshsize, dx=(1.0, 1.0, 1.0))
-
-    #macro scale
+    return grid, P, M
+end
+# ### Macro Scale
+# Setting up the grid parameters and generate a macro scale grid. 
+#
+function macro_scale_pre()
     element_number = (1, 1, 1)
     x̄_l = Vec{3, Float64}((-50, -50, -50))
     x̄_r = Vec{3, Float64}((50, 50, 50))
     grid_macro = generate_grid(Tetrahedron, element_number , x̄_l, x̄_r)
-
-    #solve
-    rve = RVE(grid, P, M)
+    return grid_macro
+end
+# ## Solve
+# Construct an object `RVESetup` named `setup_rve` with relevent infomation for solving the RVE problem.
+#
+# Due to the linearity of the problem, gloable stiffness matrix `K` and gloable mass matrix `M` as well as gloable right hand side vector `f` remain the same
+# through out the time stepping. Perform the assembly for constructing the system matrices K, M, and right hand side vector f.
+#
+# Create an appropriate boundary condition using object `MacroBCParams`
+#
+# Solve the time dependent problem macro scale problem. A corresponding RVE would be solved at each quadrature point in macro scale problem. 
+#
+# A visualization of results from both fine scale and macro scale problem can use `animate_combined_result`
+function solve(grid_rve, P, M, grid_macro, Δt, t_total)
+    rve = RVE(grid_rve, P, M)
     setup_rve = prepare_setup(rve)
     assemble_K_M_f!(setup_rve)
     bc = MacroBCParams(Vec{3}((0.01, 0.0, 0.0)), Vec{3}((0.0, 0.0, 0.0)), 1.0, 0.0, "left", "right")
     res, res_rve, setup = solve_macro_problem(grid_macro, setup_rve, rve, bc, Δt=Δt, t_total=t_total)
-
-    #file, fig, anim = animate_combined_result(res, res_rve, setup, file_name="Myresult.mp4", scale=1000.0)
 end
-
+# Construct the main function
+function example()
+    grid_rve, P, M = sub_scale_pre()
+    grid_macro = macro_scale_pre()
+    Δt = 1e-6
+    t_total = 1e-5
+    solve(grid_rve, P, M, grid_macro, Δt, t_total)
+end
 # Run the simulation
-example(1e-6, 1e-5)
-#
-# A visualization of the results from both sub scale and macro scale along the time is possible by passing the function ``animated_combined_result``. An example is shown as follows.
+example()
 # 
 #
 # 
